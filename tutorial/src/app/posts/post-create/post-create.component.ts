4c59617ecd521ddb4cb3a664a7bee587932ce27c
JoeY34kaze <Ragnar1724@gmail.com>
2020-07-12 07:23:40 -0700

    25 - Post model

--- tutorial/src/app/posts/post-create/post-create.component.ts
+++ tutorial/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core'
-
+import {Post} from '../post.model';
 @Component({
   selector : 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -10,10 +10,10 @@ export class PostCreateComponent{
 
   enteredContent='';
   enteredTitle='';
-  @Output() postCreated = new EventEmitter();//@Output deklarira ta event kot event kterga lahko poslusas od zunaj (samo od direktnega starsa!!! app.component.html)
+  @Output() postCreated = new EventEmitter<Post>();//@Output deklarira ta event kot event kterga lahko poslusas od zunaj (samo od direktnega starsa!!! app.component.html)
 
   onAddPost(){
-    const post = {
+    const post: Post = {
       title: this.enteredTitle ,
       content : this.enteredContent
     }
