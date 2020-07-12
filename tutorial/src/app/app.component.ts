e6969ce0c51e164268532249d8dc585aa3438432
JoeY34kaze <Ragnar1724@gmail.com>
2020-07-12 01:55:50 -0700

    24

--- tutorial/src/app/app.component.ts
+++ tutorial/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorial';
+
+  storedPosts=[];
+  onPostAdded(post){
+    this.storedPosts.push(post);
+  }
 }
