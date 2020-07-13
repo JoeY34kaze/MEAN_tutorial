f940f1d343d30695c9ba65c80bbb16d9bf69d8a2
JoeY34kaze <Ragnar1724@gmail.com>
2020-07-13 05:16:08 -0700

    62

---
+++ tutorial/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+//route js object for which url for which our app should be presented
+const routes : Routes = [
+  {path: '', component: PostListComponent},//if localhost -> load component of type PostListComponent that we wrote before
+  {path: 'create', component: PostCreateComponent}//if /create -> load createComponent
+];
+
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports : [RouterModule]
+
+})
+
+export class AppRoutingModule{}
